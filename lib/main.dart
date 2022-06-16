76a425974f34c3c2dc18c4b5f4e0b9b9aaed9266
hellodit33 <45536957+hellodit33@users.noreply.github.com>
2022-06-16 14:42:09 +0200

    verify

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hint/views/login_view.dart';
 import 'package:hint/views/register_view.dart';
+import 'package:hint/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -34,7 +35,18 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            /* final user = FirebaseAuth.instance.currentUser;
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('the email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
+          /*
               if (user?.emailVerified ?? false) {
                 return const Text('done');
               } else {
@@ -42,7 +54,6 @@ class HomePage extends StatelessWidget {
               }
               */
 
-            return const LoginView();
           default:
             return const CircularProgressIndicator();
         }
