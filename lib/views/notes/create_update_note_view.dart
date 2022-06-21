759d248c6a3eaeac8ffa3d5aabe6a29a20f41276
hellodit33 <pradetelodie@gmail.com>
2022-06-21 13:59:55 +0200

    share function

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hint/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:hint/utilities/generics/get_arguments.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../services/auth/auth_service.dart';
 import '../../services/cloud/cloud_note.dart';
@@ -93,6 +95,17 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(onPressed: () async {
+            final text =_textController.text;
+            if(_note == null || text.isEmpty) {
+await showCannotShareEmptyNoteDialog(context);
+            } else {
+              Share.share(text);
+            }
+          },
+          icon: const Icon(Icons.share),),
+        ]
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
